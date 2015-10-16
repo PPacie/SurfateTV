59b48ad9338f34ac86b510bc96a7a3d9b370ae01
Pablo Paciello <ppacie@gmail.com>
2015-10-16 20:50:29 -0300

    1st commit.

---
+++ SurfateTV/CameraCell.swift
@@ -0,0 +1,15 @@
+//
+//  CameraCell.swift
+//  SurfateTV
+//
+//  Created by Pablo Surfate on 10/16/15.
+//  Copyright © 2015 Pablo Paciello. All rights reserved.
+//
+
+import UIKit
+
+class CameraCell: UICollectionViewCell {
+    
+    @IBOutlet weak var camImage: UIImageView!
+    @IBOutlet weak var camTitle: UILabel!
+}
---
+++ SurfateTV/SurfCamera.swift
@@ -0,0 +1,13 @@
+//
+//  SurfCamera.swift
+//  SurfateTV
+//
+//  Created by Pablo Surfate on 10/16/15.
+//  Copyright © 2015 Pablo Paciello. All rights reserved.
+//
+
+import Foundation
+
+class SurfCamera {
+    
+}
