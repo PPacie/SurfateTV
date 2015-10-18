a00951ee8faaf68db2cfd3cd04977911cc18478e
Pablo Paciello <ppacie@gmail.com>
2015-10-18 01:48:00 -0200

    CollectionView StoryBoard adjustments.

--- SurfateTV/CameraCell.swift
+++ SurfateTV/CameraCell.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class CameraCell: UICollectionViewCell {
     
-    @IBOutlet weak var camImage: UIImageView!
-    @IBOutlet weak var camTitle: UILabel!
+    @IBOutlet weak var camImage: UIImageView?
+    @IBOutlet weak var camTitle: UILabel?
     
     var surfCam: SurfCamera? {
         didSet {
-            camTitle.text = surfCam?.camTitle
+            //Clean current cell content
+            camImage?.image = nil
+            camTitle?.text = nil
+            
+            //Update Camera Title
+            self.camTitle?.text = self.surfCam?.camTitle
+            
             //Once we get the imageData from surfCam.camImage, we proceed to update the image in the cell.
             let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
             dispatch_async(dispatch_get_global_queue(qos , 0)) { () -> Void in
                 guard let imageData = NSData(contentsOfURL: NSURL(string:(self.surfCam?.camImage)!)!) else { return }
                 let image = UIImage(data:imageData)
-                //Update UI
+                //Update the UI asyncrhonically in main Thread.
                 dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                    //Update the UI asyncrhonically in main Thread.
-                    self.camImage.image = image
+                    self.camImage?.image = image
                 }
             }
         }
